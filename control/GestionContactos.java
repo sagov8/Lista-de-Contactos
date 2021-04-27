216dddf3de22684174351876ac18726603ee0e15
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-04-27 13:24:10 -0500

    Creación de proyecto
    MVC creado y 3 clases Persona, Control e Interfaz gráfica

---
+++ control/GestionContactos.java
@@ -0,0 +1,31 @@
+
+package control;
+
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Persona;
+
+/**
+ *
+ * @author sagov8
+ */
+public class GestionContactos {
+    
+    List<Persona> listacontactos = new ArrayList<>();
+    
+    public void agregarPersona(Persona p){
+        listacontactos.add(p);
+    }
+    
+    public void modificarPersona(Persona p){
+       
+    }
+    
+    public void consultarPersona(Persona p){
+        
+    }
+    
+    public void eliminarPersona(Persona p){
+        
+    }
+}
---
+++ modelo/Persona.java
@@ -0,0 +1,57 @@
+package modelo;
+
+/**
+ *
+ * @author sagov8
+ */
+public class Persona {
+
+    private String id;
+    private String nombre;
+    private String edad;
+    private String genero;
+    private String numeroTelefonico;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getNumeroTelefonico() {
+        return numeroTelefonico;
+    }
+
+    public void setNumeroTelefonico(String numeroTelefonico) {
+        this.numeroTelefonico = numeroTelefonico;
+    }
+    
+    
+    
+}
