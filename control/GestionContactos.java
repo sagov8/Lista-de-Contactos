ae5b9d6e003650c7d91046f71ed2dab32a036924
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-04-27 20:41:37 -0500

    Update GestionContactos.java
    Bug fixes.

--- control/GestionContactos.java
+++ control/GestionContactos.java
@@ -1,4 +1,3 @@
-
 package control;
 
 import java.util.ArrayList;
@@ -13,68 +12,66 @@ import modelo.Persona;
  * @author sagov8
  */
 public class GestionContactos {
-    
+
     List<Persona> lista_contactos = new ArrayList<>();
     Persona persona;
-    
-    public void registrarPersona(String id, String nom, String años, String gene, String telefono){
-        persona =new Persona();
+
+    public void registrarPersona(String id, String nom, String años, String gene, String telefono) {
+        persona = new Persona();
         persona.setId(id);
         persona.setNombre(nom);
         persona.setEdad(años);
         persona.setGenero(gene);
         persona.setNumeroTelefonico(Integer.parseInt(telefono));
         lista_contactos.add(persona);
-        JOptionPane.showMessageDialog(null," CONTACTO REGISTRADO");
+        JOptionPane.showMessageDialog(null, " CONTACTO REGISTRADO");
     }
-    
-    public void modificarPersona( String id,String nom,String años, String gene,String numero){
-          int pos = Integer.parseInt(id); 
-        
+
+    public void modificarPersona(String id, String nom, String años, String gene, String numero) {
+        int pos = Integer.parseInt(id);
+
         //persona.setId(id);
         persona.setNombre(nom);
         persona.setEdad(años);
         persona.setGenero(gene);
         persona.setNumeroTelefonico(Integer.parseInt(numero));
-        
-        lista_contactos.add(pos,persona);
+
+        lista_contactos.add(pos, persona);
         JOptionPane.showMessageDialog(null, "EL CONTACTO SE HA MODIFICADO");
-       
+
     }
-    
-    public void listaPersona(JTextArea area){
-        
-         area.setText("");
+
+    public void listaPersona(JTextArea area) {
+
+        area.setText("");
         for (int posicion = 0; posicion < lista_contactos.size(); posicion++) {
-            Persona  persona = lista_contactos.get(posicion);
+            Persona persona = lista_contactos.get(posicion);
             area.append("\nContacto: ");
             area.append("\n ID: " + persona.getId());
             area.append("\nNombre: " + persona.getNombre());
             area.append("\nEdad: " + persona.getEdad());
-            area.append("\nGenero: "+ persona.getGenero());
-            area.append("\nNumero de telefono:" +persona.getNumeroTelefonico());
+            area.append("\nGenero: " + persona.getGenero());
+            area.append("\nNumero de telefono:" + persona.getNumeroTelefonico());
         }
-        
+
     }
-    
-    public void eliminarPersona(Persona p){
-        
+
+    public void eliminarPersona(Persona p) {
+
     }
-    
-    public void consultarPersona(JTextField id, JTextField nombre, 
-            JTextField edad, JTextField genero, JTextField numTel){
-        
+
+    public void consultarPersona(JTextField id, JTextField nombre,
+            JTextField edad, JTextField genero, JTextField numTel) {
+
         String identificacion = String.valueOf(id);
-        for(Persona persona: lista_contactos){
-            if(persona.getId() == identificacion){
+        for (Persona persona : lista_contactos) {
+            if (persona.getId().equals(identificacion)) {
                 nombre.setText(persona.getNombre());
                 edad.setText(persona.getEdad());
                 genero.setText(persona.getGenero());
                 numTel.setText(String.valueOf(persona.getNumeroTelefonico()));
-            }else{
-                JOptionPane.showConfirmDialog(null, "No se ha encontrado la persona");
             }
         }
-        
+
     }
 }
